d06b8a0ab600b2ef3dc292e5da1bf2a65e4dfec9
Yongchol Shin <yongchols@gmail.com>
2021-01-22 01:23:54 +0000

    modify frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,25 @@
-import React from 'react';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import Link from '@material-ui/core/Link';
-import ProTip from './ProTip';
+import React, { Component } from 'react';
+import './App.css';
+import Customer from './components/Customer';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+const customer = {
+  'name': 'Yongchol Shin',
+  'birthday': '830122',
+  'gender': 'male',
+  'job': 'Engineer3'
 }
 
-export default function App() {
-  return (
-    <Container maxWidth="sm">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Create React App v4-beta example
-        </Typography>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Customer
+        name={customer.name}
+        birthday={customer.birthday}
+        gender={customer.gender}
+        job={customer.job}
+      />
+    );
+  }
 }
+
+export default App;
